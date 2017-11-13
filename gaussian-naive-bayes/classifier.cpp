798ac9b0aa3a4a39d31293f81881a7c36e229322
Ahmad Khatib <ackhatib@gmail.com>
2017-11-13 20:00:00 +0100

    Implement training method.

--- gaussian-naive-bayes/classifier.cpp
+++ gaussian-naive-bayes/classifier.cpp
@@ -3,29 +3,59 @@
 #include <fstream>
 #include <math.h>
 #include <vector>
+#include <map>
 #include "classifier.h"
 
-GNB::GNB() {}
+using namespace std;
+
+GNB::GNB() {
+  // Initializing the distributions
+  for (int i = 0; i < labels.size(); i++) {
+    distributions[labels[i]] = {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}};
+  }
+}
 
 GNB::~GNB() {}
 
 void GNB::train(vector<vector<double>> data, vector<string> labels) {
-
   /*
-    Trains the classifier with N data points and labels.
+   *  Refer to https://www.johndcook.com/blog/standard_deviation/
+   *  for accurately calculating running mean and variance
+   *   Mk = Mk-1+ (xk – Mk-1)/k
+   *   Sk = Sk-1 + (xk – Mk-1)*(xk – Mk)
+   * 
+   *  Trains the classifier with N data points and labels.
+   *
+   *  INPUTS
+   *  data - array of N observations
+   *    - Each observation is a tuple with 4 values: s, d, s_dot and d_dot.
+   *    - Example : [
+   *        [3.5, 0.1, 5.9, -0.02],
+   *        [8.0, -0.3, 3.0, 2.2],
+   *        ...
+   *      ]
+   *
+   *  labels - array of N labels
+   *    - Each label is one of "left", "keep", or "right".
+   */
 
-    INPUTS
-    data - array of N observations
-      - Each observation is a tuple with 4 values: s, d, s_dot and d_dot.
-      - Example : [
-          [3.5, 0.1, 5.9, -0.02],
-          [8.0, -0.3, 3.0, 2.2],
-          ...
-        ]
+  for (int i = 0; i < data.size(); i++) {
+    string label = labels[i];
+    vector<double> obs = data[i];
 
-    labels - array of N labels
-      - Each label is one of "left", "keep", or "right".
-  */
+    for (int j = 0; j < distributions[label].size(); j++) {
+      float count = distributions[label][j][0] + 1;
+      float mean  = distributions[label][j][1];
+      float stdd  = distributions[label][j][2];
+
+      float new_mean = mean + (obs[j] - mean) / count;
+      float new_stdd = stdd + (obs[j] - mean)*(obs[j] - new_mean);
+
+      distributions[label][j][0] = count;
+      distributions[label][j][1] = new_mean;
+      distributions[label][j][2] = new_stdd;
+    }
+  }
 
 }
 
@@ -47,6 +77,6 @@ string GNB::predict(vector<double> sample) {
     TODO - complete this
   */
 
-  return this->possible_labels[1];
+  return labels[1];
 }
 
--- gaussian-naive-bayes/classifier.h
+++ gaussian-naive-bayes/classifier.h
@@ -6,13 +6,21 @@
 #include <fstream>
 #include <math.h>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 class GNB {
   public:
 
-    vector<string> possible_labels = {"left","keep","right"};
+    vector<string> labels = {"left", "keep", "right"};
+
+    /*
+     * For each class:
+     *       s       |      d       |    sdot      |    ddot
+     *  {#, mu, sig} | {#, mu, sig} | {#, mu, sig} | {#, mu, sig}
+     */
+    map<string, vector<vector<float>>> distributions;
 
     GNB();
 
